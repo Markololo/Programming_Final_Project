38b3cb706421f72aeda4c6ee748f4ce8aa235ac0
Markololo <markololo2468@gmail.com>
2024-05-16 14:34:26 -0400

    Initializing the main branch

---
+++ src/main/java/org/example/Assignment.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Assignment {
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private int maxScore;
+    private double assignmentAvg;
+    private int[] scores;
+    private static int nextId;
+}
