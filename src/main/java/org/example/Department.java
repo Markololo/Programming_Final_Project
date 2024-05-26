35b663a26910c6370c2e7c7ba8bf916a958ceba5
%username% <markololo2468@gmail.com>
2024-05-25 23:21:20 -0400

    Finished Department class

--- src/main/java/org/example/Department.java
+++ src/main/java/org/example/Department.java
@@ -1,25 +1,38 @@
 package org.example;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Department {
     private String departmentId;
     private String departmentName;
-    private static int nextId = 1;
+    private static int nextId;
 
     public static boolean validateDepartmentName(String departmentName) {
         boolean isValid;
 
         for (int i = 0; i < departmentName.length(); i++) {
             char c = departmentName.charAt(i);
-
+            isValid = Character.isAlphabetic(c) || Character.isSpaceChar(c);
+            if (!isValid) {
+                return false;
+            }
         }
         return true;
     }
+    public Department(String departmentId, String departmentName) {
+        if (validateDepartmentName(departmentName)){
+            this.departmentId = departmentId;
+            this.departmentName = departmentName;
+        } else {
+            this.departmentId = null;
+            this.departmentName = null;
+        }
+    }
 }
-/*
-static boolean validateDepartmentName(String departmentName) // checks if a department name is valid or not, a department name should only contain letters or space
-Constructor // if the departmentName is invalid, create the object with everything as null;
-toString
-equals
-getter
-setter
- */
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println();
     }
+
 //    public static String toTitleCase(String strIn) {
 //        strIn.toLowerCase();
 //        String strOut = "";
